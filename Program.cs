36bddf97b98c9e8eb59b13fba29e1ee8944eeb9f
Marina <marinafriendrix@gmail.com>
2022-09-18 16:28:56 +0300

    решение задачи

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// var resultArr = existArr.Where(x=>x.Length < 4).ToArray();
+
 
 string[] Array1;
 Array1 = new string[] {"danmark", "rz", "2", "500", "moscow"};
@@ -12,3 +14,16 @@ for (int i = 0; i < Array1.Length; i++)
 
     }
     Console.WriteLine();
+for (int i = 0; i < Array1.Length; i++)
+{
+    if (Array1[i].Length < 3)
+    {
+        Console.WriteLine(Array1[i]);
+    }
+    
+}
+
+    
+    
+    
+
